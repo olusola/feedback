f9a85bb0025dda26cfeea25d5159ab8438d6c870
olusola <olusola2509@gmail.com>
2021-01-11 19:30:20 +0000

    clean up

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,18 +1,10 @@
 import '../styles/globals.css'
-import { Amplify } from 'aws-amplify'
+import { Amplify, Auth } from 'aws-amplify'
+import config from '../src/aws-exports'
 import { ChakraProvider } from '@chakra-ui/react'
 
-
-const awsmobile = {
-  "aws_project_region": process.env.aws_project_region,
-  "aws_cognito_identity_pool_id": process.env.aws_cognito_identity_pool_id,
-  "aws_cognito_region": process.env.aws_cognito_region,
-  "aws_user_pools_id": process.env.aws_user_pools_id,
-  "aws_user_pools_web_client_id": process.env.aws_user_pools_web_client_id,
-  "oauth": {}
-};
-
-Amplify.configure({...awsmobile, ssr: true})
+Amplify.configure({...config, ssr: true})
+Auth.configure({...config, ssr: true})
 
 function MyApp({ Component, pageProps }) {
   return <ChakraProvider>
